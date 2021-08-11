a905bdea28331ff1fa838f6b29be21bb5b4c9b49
riniwtz <riniwtzcode@gmail.com>
2021-08-11 23:05:27 +0800

    Updated Code

--- MCCJava/src/io/github/riniwtz/mcc/Main.java
+++ MCCJava/src/io/github/riniwtz/mcc/Main.java
@@ -8,6 +8,10 @@ import io.github.riniwtz.commands.KillCommand;
 import io.github.riniwtz.commands.TimeCommand;
 
 public class Main {
+	protected static final GiveCommand GIVE_COMMAND = new GiveCommand();
+	protected static final TimeCommand TIME_COMMAND = new TimeCommand();
+	protected static final KillCommand KILL_COMMAND = new KillCommand();
+
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		Player player = new Player();
@@ -26,9 +30,9 @@ public class Main {
 			if ((text.length() > 0) && (text.charAt(0) == '/')) {
 				cmd = text.split(" ");
 				switch (cmd[0]) {
-					case "/give" -> new GiveCommand().execute(cmd);
-					case "/time" -> new TimeCommand().execute(cmd);
-					case "/kill" -> new KillCommand().execute(cmd);
+					case "/give" -> GIVE_COMMAND.execute(cmd);
+					case "/time" -> TIME_COMMAND.execute(cmd);
+					case "/kill" -> KILL_COMMAND.execute(cmd);
 					default -> {
 						CommandOutputMessage.printUnknownCommandOutput();
 						CommandOutputMessage.printUnknownCommandDefaultOutput(cmd);
