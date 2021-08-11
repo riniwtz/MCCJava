81ca828b93ae6e73caa38162f9bfdf2ada8f3a89
riniwtz <riniwtzcode@gmail.com>
2021-08-11 14:51:18 +0800

    Fixed Bugs

--- MCCJava/src/io/github/riniwtz/mcc/Blocks.java
+++ MCCJava/src/io/github/riniwtz/mcc/Blocks.java
@@ -9,25 +9,23 @@ public class Blocks {
 	// FIXME - block and item initialization especially the exceptions
 	// TODO - Add all block and item names in blocks_list and items_list files
 	ArrayList<String> block = new ArrayList<>();
-	
+
 	public void initializeBlock() throws FileNotFoundException, IOException {
-		
 		File file = new File("src\\io\\github\\riniwtz\\resources\\blocks_list").getAbsoluteFile();
-		
 		Scanner scan = new Scanner(file);
-		
-		String fileContent = "";
+
+		String blockName = "";
 		while (scan.hasNextLine()) {
-			fileContent = scan.nextLine();
-			block.add(fileContent);
+			blockName = scan.nextLine();
+			block.add(blockName);
 		}
 		scan.close();
 	}
-	
+
 	public ArrayList<String> getBlocks() {
 		return block;
 	}
-	
+
 	public boolean exists(String[] cmd) {
 		for (String b : getBlocks()) {
 			if (cmd[2].equals(b)) {
--- MCCJava/src/io/github/riniwtz/mcc/Items.java
+++ MCCJava/src/io/github/riniwtz/mcc/Items.java
@@ -4,15 +4,22 @@ import java.util.ArrayList;
 public class Items {
 	// FIXME - block and item initialization especially the exceptions
 	ArrayList<String> item = new ArrayList<>();
-	public Items() {
-		item.add("stick");
-		item.add("diamond");
+	public void initializeItem() throws FileNotFoundException, IOException {
+		File file = new File("src\\io\\github\\riniwtz\\resources\\items_list").getAbsoluteFile();
+		Scanner scan = new Scanner(file);
+
+		String itemName = "";
+		while (scan.hasNextLine()) {
+			itemName = scan.nextLine();
+			item.add(itemName);
+		}
+		scan.close();
 	}
-	
+
 	public ArrayList<String> getItems() {
 		return item;
 	}
-	
+
 	public boolean exists(String[] cmd) {
 		for (String i : getItems()) {
 			if (cmd[2].equals(i)) {
