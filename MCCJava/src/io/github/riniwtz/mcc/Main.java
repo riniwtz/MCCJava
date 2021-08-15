c3694e9c61a9b3d588ea05b14055f7d23aaa1b93
riniwtz <riniwtzcode@gmail.com>
2021-08-15 22:53:39 +0800

    Added /gamemode

--- MCCJava/src/io/github/riniwtz/mcc/Main.java
+++ MCCJava/src/io/github/riniwtz/mcc/Main.java
@@ -2,16 +2,23 @@ package io.github.riniwtz.mcc;
 import java.util.NoSuchElementException;
 import java.util.Scanner;
 
-import io.github.riniwtz.commands.CommandOutputMessage;
-import io.github.riniwtz.commands.GiveCommand;
-import io.github.riniwtz.commands.KillCommand;
-import io.github.riniwtz.commands.TimeCommand;
+import io.github.riniwtz.commands.*;
+
+
+/*
+- GiveCommand 98% done
+- Blocks and Items are ArrayList and is not full list
+- TimeCommand (WIP)
+- KillCommand (WIP)
+- GameModeCommand (WIP)
+ */
 
 public class Main {
 	// FIXME - Java Reflections (WIP)
-	protected static final GiveCommand GIVE_COMMAND = new GiveCommand();
-	protected static final TimeCommand TIME_COMMAND = new TimeCommand();
-	protected static final KillCommand KILL_COMMAND = new KillCommand();
+	private static final GiveCommand GIVE_COMMAND = new GiveCommand();
+	private static final TimeCommand TIME_COMMAND = new TimeCommand();
+	private static final KillCommand KILL_COMMAND = new KillCommand();
+	private static final GameModeCommand GAME_MODE_COMMAND = new GameModeCommand();
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
@@ -34,6 +41,7 @@ public class Main {
 					case "/give" -> GIVE_COMMAND.execute(cmd);
 					case "/time" -> TIME_COMMAND.execute(cmd);
 					case "/kill" -> KILL_COMMAND.execute(cmd);
+					case "/gamemode" -> GAME_MODE_COMMAND.execute(cmd);
 					default -> {
 						CommandOutputMessage.printUnknownCommandOutput();
 						CommandOutputMessage.printUnknownCommandDefaultOutput(cmd);
