517a66aaaad62064994f5280173947b8c30b4fc9
riniwtz <riniwtzcode@gmail.com>
2021-08-17 16:02:19 +0800

    Added GameMode methods

--- MCCJava/src/io/github/riniwtz/mcc/World.java
+++ MCCJava/src/io/github/riniwtz/mcc/World.java
@@ -1,21 +1,33 @@
 package io.github.riniwtz.mcc;
-
 public class World implements Time {
 	private String worldName = "My World";
 	private float time = 0.0F;
 
+	public GameMode gamemode;
+	public enum GameMode {
+		ADVENTURE,
+		CREATIVE,
+		SPECTATOR,
+		SURVIVAL
+	}
+	public void setGameMode(GameMode gamemode) {
+		this.gamemode = gamemode;
+	}
+	public String getGameModeName() {
+		return gamemode.name();
+	}
+	public GameMode getGameMode() {
+		return gamemode;
+	}
 	public World() {
 		boolean isNight = getTime() >= 13000;
 	}
-
 	public void addTime(float time) {
 		this.time += time;
 	}
-
 	public void setTime(float time) {
 		this.time = time;
 	}
-
 	public float getTime() {
 		float maxTime = 24000F;
 		if (time >= maxTime)
@@ -23,5 +35,4 @@ public class World implements Time {
 
 		return Math.round(time);
 	}
-
 }
