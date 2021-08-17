3da4c6e5915fc55899e6ba3ba32c2428a2bc75a4
riniwtz <riniwtzcode@gmail.com>
2021-08-17 15:56:17 +0800

    Added GameMode Enum and GameModeCommand is done

--- MCCJava/src/io/github/riniwtz/commands/GameModeCommand.java
+++ MCCJava/src/io/github/riniwtz/commands/GameModeCommand.java
@@ -1,35 +1,47 @@
 package io.github.riniwtz.commands;
+import io.github.riniwtz.mcc.World;
 
-public class GameModeCommand extends BaseCommand {
-    GameMode gamemode;
-    enum GameMode {
-        ADVENTURE,
-        CREATIVE,
-        SPECTATOR,
-        SURVIVAL
-    }
-
-	private static final int MINIMUM_ARGUMENT = 2;
+public class GameModeCommand extends AbstractBaseCommand {
+    private static final int MINIMUM_ARGUMENT = 2;
     private static final int MAXIMUM_ARGUMENT = 3;
-
-    protected void setGameMode(GameMode gamemode) {
-        this.gamemode = gamemode;
-    }
-
+    private String gamemode;
     @Override
     public void execute(String[] cmd) {
+        if (cmd.length > 1) gamemode = cmd[1];
     	if (!(hasCommandHandlerError(cmd))) {
-    		
+            switch (gamemode) {
+                case "adventure" -> {
+                    world.setGameMode(World.GameMode.ADVENTURE);
+                    CommandOutputMessage.printSetGameModeMessageOutput(world);
+                }
+                case "creative" -> {
+                    world.setGameMode(World.GameMode.CREATIVE);
+                    CommandOutputMessage.printSetGameModeMessageOutput(world);
+                }
+                case "spectator" -> {
+                    world.setGameMode(World.GameMode.SPECTATOR);
+                    CommandOutputMessage.printSetGameModeMessageOutput(world);
+                }
+                case "survival" -> {
+                    world.setGameMode(World.GameMode.SURVIVAL);
+                    CommandOutputMessage.printSetGameModeMessageOutput(world);
+                }
+                default -> {
+                    CommandOutputMessage.printIncorrectArgumentCommandMessageOutput();
+                    CommandOutputMessage.printUnknownCommandDefaultMessageOutput(cmd);
+                }
+            }
     	}
     }
-
     @Override
     protected boolean hasCommandHandlerError(String[] cmd) {
-        if ((cmd.length < MINIMUM_ARGUMENT) && (cmd.length > MAXIMUM_ARGUMENT)) {
-
-            CommandOutputMessage.printUnknownCommandOutput();
-            CommandOutputMessage.printUnknownCommandDefaultOutput(cmd);
+        if (cmd.length < MINIMUM_ARGUMENT) {
+            CommandOutputMessage.printUnknownCommandMessageOutput();
+            CommandOutputMessage.printUnknownCommandDefaultMessageOutput(cmd);
             return true;
+        }
+        if (!(world.getGameMode() == null)) {
+            return gamemode.equals(world.getGameModeName().toLowerCase());
         }
         return false;
     }
