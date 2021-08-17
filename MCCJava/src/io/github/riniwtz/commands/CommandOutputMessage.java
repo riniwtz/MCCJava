03f828bb13fda60618dd5ca942dbec9c2d605802
riniwtz <riniwtzcode@gmail.com>
2021-08-17 15:55:07 +0800

    Added printSetGameModeMessageOutput method

--- MCCJava/src/io/github/riniwtz/commands/CommandOutputMessage.java
+++ MCCJava/src/io/github/riniwtz/commands/CommandOutputMessage.java
@@ -3,19 +3,23 @@ import io.github.riniwtz.mcc.Player;
 import io.github.riniwtz.mcc.World;
 
 public class CommandOutputMessage {
+	public static void printSetGameModeMessageOutput(World world) {
+		System.out.println("Set own game mode to " + toUpperCaseFirstChar(world.getGameModeName()) + " Mode");
+	}
+
 	public static void printMessageOutput(Player player, String message) {
 		System.out.println(("<") + (player.getName()) + (">") + (" ") + (message));
 	}
 	
-	public static void printGivePlayerAmountLimitOutput(String item) {
+	public static void printGivePlayerAmountLimitMessageOutput(String item) {
 		System.out.println(("Can't give more than 6400 of [") + (getConvertItemIDToItemName(item)) + ("]"));
 	}
 	
-	public static void printGivePlayerItemOutput(String item, int amount, Player player) {
+	public static void printGivePlayerItemMessageOutput(String item, int amount, Player player) {
 		System.out.println(("Gave ") + (amount) + (" ") + ("[") + (getConvertItemIDToItemName(item)) + ("] to ") + (player.getName()));
 	}
 
-	public static void printTimeOutput(String command, World world) {
+	public static void printTimeMessageOutput(String command, World world) {
 		if (command.equals("set") || command.equals("add"))
 			System.out.println("Set the time to " + (int) world.getTime());
 		
@@ -23,46 +27,46 @@ public class CommandOutputMessage {
 			System.out.println("The time is " + (int) world.getTime()); // getTime() should be query time (WIP)
 	}
 	
-	public static void printKillPlayerOutput(String name) {
+	public static void printKillPlayerMessageOutput(String name) {
 		System.out.println((name) + (" fell out of the world"));
 		System.out.println(("Killed ") + (name));
 	}
 	
-	public static void printExpectedFloatOutput() {
+	public static void printExpectedFloatMessageOutput() {
 		System.out.println("Expected float");
 	}
 	
-	public static void printInvalidIntegerOutput(long amount) {
+	public static void printInvalidIntegerMessageOutput(long amount) {
 		System.out.println(("Invalid integer '") + (amount) + ("'"));
 	}
 	
-	public static void printExpectedIntegerOutput() {
+	public static void printExpectedIntegerMessageOutput() {
 		System.out.println("Expected integer");
 	}
 	
-	public static void printIntegerIsZeroOutput() {
+	public static void printIntegerIsZeroMessageOutput() {
 		System.out.println("Integer must not be less than 1, found 0");
 	}
 	
-	public static void printUnknownItemOutput(String itemName) {
+	public static void printUnknownItemMessageOutput(String itemName) {
 		System.out.println("Unknown item 'minecraft:" + (itemName) + "'");
 	}
 	
-	public static void printUnknownCommandOutput() {
+	public static void printUnknownCommandMessageOutput() {
 		System.out.println("Unknown or incomplete command, see below for error");
 	}
 	
-	public static void printUnknownCommandDefaultOutput(String[] cmd) {
+	public static void printUnknownCommandDefaultMessageOutput(String[] cmd) {
 		StringBuilder out = new StringBuilder();
 		for (String c : cmd) out.append(c).append(" ");
 		System.out.print(out.substring(0, out.length() - 1) + ("<--[HERE]\n"));
 	}
 
-	public static void printNoPlayerFoundOutput() {
+	public static void printNoPlayerFoundMessageOutput() {
 		System.out.println("No player was found");
 	}
 
-	protected static void printIncorrectArgumentCommandOutput() {
+	public static void printIncorrectArgumentCommandMessageOutput() {
 		System.out.println("Incorrect argument for command");
 	}
 
@@ -70,13 +74,13 @@ public class CommandOutputMessage {
 		String[] itemName = itemID.split("_");
 		StringBuilder itemIDBuilder = new StringBuilder();
 		for (String i : itemName) {
-			itemIDBuilder.append(getUpperCaseFirstChar(i)).append(" ");
+			itemIDBuilder.append(toUpperCaseFirstChar(i)).append(" ");
 		}
 		itemID = itemIDBuilder.toString();
 		return itemID.substring(0, itemID.length() - 1);
 	}
 
-	private static String getUpperCaseFirstChar(String text) {
-		return (text.substring(0, 1).toUpperCase()) + (text.substring(1));
+	private static String toUpperCaseFirstChar(String text) {
+		return (text.substring(0, 1).toUpperCase()) + (text.substring(1).toLowerCase());
 	}
 }
