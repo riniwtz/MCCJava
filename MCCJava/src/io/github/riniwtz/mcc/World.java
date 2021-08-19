979fe212ce1f7f878062912629b4f5acf1d7664f
riniwtz <riniwtzcode@gmail.com>
2021-08-19 21:28:47 +0800

    Now supports BigDecimal class which includes high precision calculation decimals

--- MCCJava/src/io/github/riniwtz/mcc/World.java
+++ MCCJava/src/io/github/riniwtz/mcc/World.java
@@ -1,9 +1,13 @@
 package io.github.riniwtz.mcc;
 
+import java.math.BigDecimal;
+import java.math.MathContext;
+import java.math.RoundingMode;
+
 public class World implements Time {
 	private String worldName = "My World";
 	private int gameTicks = 0;
-	private double timeDouble = getTime();
+	private double time;
 	private boolean stopTime = false;
 
 	public GameMode gamemode;
@@ -23,24 +27,38 @@ public class World implements Time {
 		return gamemode;
 	}
 	public World() {
-		boolean isNight = getTime() >= 13000;
+		boolean isNight = false;
 	}
 
-	public void addTime(double timeDouble) {
-		this.timeDouble += (float) timeDouble;
+	public void addTime(double time) {
+		this.time += time;
 	}
-	public void setTime(double timeDouble) {
-		this.timeDouble = timeDouble;
-		gameTicks = (int) timeDouble;
+	public void setTime(double time) {
+		this.time = time;
+		gameTicks = (int) time;
 	}
-	public double getTime() {
+	/*
+	 * 	getTime() method uses a BigDecimal class which consists of an arbitrary precision integer unscaled value and a 32-bit integer scale.
+	 * 	If zero or positive, the scale is the number of digits to the right of the decimal point. If negative, the unscaled value of the
+	 * 	number is multiplied by ten to the power of the negation of the scale. The value of the number represented by the BigDecimal
+	 * 	is therefore (unscaledValue × 10-scale).
+	 *
+	 * https://docs.oracle.com/javase/8/docs/api/java/math/BigDecimal.html
+	 */
+	public BigDecimal getTime() {
 		final double MAX_TIME = 24000D;
-		if (timeDouble >= MAX_TIME) {
-			double firstComputation = this.timeDouble / MAX_TIME;
-			double secondComputation = Math.floor(this.timeDouble / MAX_TIME);
-			timeDouble = (firstComputation - secondComputation) * MAX_TIME;
+		BigDecimal bigDecimal;
+		if (time >= MAX_TIME) {
+			bigDecimal = BigDecimal.valueOf(time)
+					.divide(BigDecimal.valueOf(MAX_TIME), MathContext.DECIMAL64);
+
+			return (
+					bigDecimal.subtract(BigDecimal.valueOf(bigDecimal.intValue())))
+					.multiply(BigDecimal.valueOf(MAX_TIME))
+					.setScale(1, RoundingMode.HALF_UP
+					);
 		}
-		return Math.round(timeDouble);
+		return BigDecimal.valueOf(time);
 	}
 
 	@Override
