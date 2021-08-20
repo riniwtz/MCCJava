11ac69364b712d77d6bfeba2254ba4a7c1694641
riniwtz <riniwtzcode@gmail.com>
2021-08-20 19:58:36 +0800

    refactor code

--- MCCJava/src/io/github/riniwtz/main/Main.java
+++ MCCJava/src/io/github/riniwtz/main/Main.java
@@ -4,52 +4,94 @@ import java.util.Scanner;
 import io.github.riniwtz.commands.*;
 import io.github.riniwtz.mcc.Player;
 
-/*
-- GiveCommand 98% done
-- Blocks and Items are ArrayList and is not full list
-- TimeCommand (WIP)
-- KillCommand (WIP)
-- GameModeCommand (WIP)
- */
-
-public class Main {
+public class Main implements Runnable {
 	// FIXME - Java Reflections (WIP)
 	private static final GiveCommand GIVE_COMMAND = new GiveCommand();
 	private static final TimeCommand TIME_COMMAND = new TimeCommand();
 	private static final KillCommand KILL_COMMAND = new KillCommand();
 	private static final GameModeCommand GAME_MODE_COMMAND = new GameModeCommand();
-	public static boolean running = false;
+	private static final HelpCommand HELP_COMMAND = new HelpCommand();
 
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		Player player = new Player();
-		String text;
-		String[] cmd;
+	public static final String NAME_ID = "MCCJava";
+	public static final String AUTHOR = "riniwtz";
+	public static final String VERSION = "Version 1.0";
 
-		running = true;
-		while (running) {
-			try { text = sc.nextLine(); }
+	private static final Scanner sc = new Scanner(System.in);
+	private static final Player player = new Player();
+
+	private static void setCommand(String[] cmd) {
+		switch (cmd[0]) {
+			case "/give" -> GIVE_COMMAND.execute(cmd);
+			case "/time" -> TIME_COMMAND.execute(cmd);
+			case "/kill" -> KILL_COMMAND.execute(cmd);
+			case "/gamemode" -> GAME_MODE_COMMAND.execute(cmd);
+			case "/help" -> HELP_COMMAND.execute(cmd);
+			case "/quit" -> System.exit(0);
+			default -> {
+				CommandOutputMessage.printUnknownCommandMessageOutput();
+				CommandOutputMessage.printUnknownCommandDefaultMessageOutput(cmd);
+			}
+		}
+	}
+
+	private static void initializeName() {
+		String name;
+		boolean hasName = false;
+		do {
+			try {
+				name = sc.nextLine();
+				if (name.length() > 0) {
+					player.setName(name);
+					hasName = true;
+				}
+			}
+			catch (NoSuchElementException e) {
+				e.printStackTrace();
+			}
+		} while (!hasName);
+	}
+
+	public static void printProgramInfo() {
+		System.out.println(
+				"Minecraft Commands Java [" + NAME_ID + ": " + VERSION + "]\n"
+						+ "(c) " + AUTHOR + ". All rights reserved.\n"
+						+ "Contact: riniwtzcode@gmail.com\n"
+		);
+	}
+
+	@Override
+	public void run() {
+		printProgramInfo();
+		System.out.print("Enter your name: ");
+		initializeName();
+		System.out.println("Type a message or a command");
+		while (true) {
+			System.out.print("> ");
+			String text;
+			try {
+				text = sc.nextLine();
+			}
 			catch (NoSuchElementException e) {
 				e.printStackTrace();
 				break;
 			}
 			// TODO - Add all commands that are possible from Minecraft commands
-			if ((text.length() > 0) && (text.charAt(0) == '/')) {
-				cmd = text.split(" ");
-				switch (cmd[0]) {
-					case "/time" -> TIME_COMMAND.execute(cmd);
-					case "/kill" -> KILL_COMMAND.execute(cmd);
-					case "/gamemode" -> GAME_MODE_COMMAND.execute(cmd);
-					case "/quit" -> System.exit(0);
-					default -> {
-						CommandOutputMessage.printUnknownCommandMessageOutput();
-						CommandOutputMessage.printUnknownCommandDefaultMessageOutput(cmd);
-					}
+			String[] cmd;
+			if (text.length() > 0) {
+				if (text.charAt(0) == '/') {
+					cmd = text.split(" ");
+					setCommand(cmd);
+				}
+				else {
+					CommandOutputMessage.printMessageOutput(player, text);
 				}
 			}
-			else
-				CommandOutputMessage.printMessageOutput(player, text);
 		}
 		sc.close();
+	}
+
+	public static void main(String[] args) {
+		Thread thread = new Thread(new Main());
+		thread.start();
 	}
 }
