bd6eae5264233801ee3344c2692e99a105491b5c
riniwtz <riniwtzcode@gmail.com>
2021-08-22 22:20:59 +0800

    Block and Items fix

--- MCCJava/src/io/github/riniwtz/commands/CommandOutputMessage.java
+++ MCCJava/src/io/github/riniwtz/commands/CommandOutputMessage.java
@@ -1,18 +1,22 @@
 package io.github.riniwtz.commands;
+import io.github.riniwtz.mcc.Blocks;
+import io.github.riniwtz.mcc.Items;
 import io.github.riniwtz.mcc.Player;
 import io.github.riniwtz.mcc.World;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.HashMap;
 import java.util.Objects;
 
 public class CommandOutputMessage {
-	protected static BufferedReader helpCommandList = new BufferedReader(new InputStreamReader(Objects.requireNonNull(CommandOutputMessage.class.getResourceAsStream("/io/github/riniwtz/resources/help_command_list"))));
+	protected static final BufferedReader HELP_COMMAND_LIST = new BufferedReader(new InputStreamReader(Objects.requireNonNull(CommandOutputMessage.class.getResourceAsStream("/io/github/riniwtz/resources/help_command_list"))));
+
 	public static void printHelpCommandListMessageOutput() {
 		try {
 			String line;
-			while ((line = helpCommandList.readLine()) != null) {
+			while ((line = HELP_COMMAND_LIST.readLine()) != null) {
 				System.out.println(line);
 			}
 		} catch (NumberFormatException | IOException e) {
@@ -35,15 +39,21 @@ public class CommandOutputMessage {
 	public static void printMessageOutput(Player player, String message) {
 		System.out.println(("<") + (player.getName()) + (">") + (" ") + (message));
 	}
-	
-	public static void printGivePlayerAmountLimitMessageOutput(String item) {
-		System.out.println(("Can't give more than 6400 of [") + (getConvertItemIDToItemName(item)) + ("]"));
-	}
-	
-	public static void printGivePlayerItemMessageOutput(String item, int amount, Player player) {
-		System.out.println(("Gave ") + (amount) + (" ") + ("[") + (getConvertItemIDToItemName(item)) + ("] to ") + (player.getName()));
-	}
 
+	private static final Blocks BLOCK_MAP = new Blocks();
+	private static final Items ITEM_MAP = new Items();
+	public static void printGivePlayerAmountLimitMessageOutput(String item) {
+		if (BLOCK_MAP.exists(BLOCK_MAP.getBlockMap().get(item)))
+			System.out.println(("Can't give more than 6400 of [") + (BLOCK_MAP.getBlockMap().get(item)) + ("]"));
+		else if (ITEM_MAP.exists(ITEM_MAP.getItemMap().get(item)))
+			System.out.println(("Can't give more than 6400 of [") + (ITEM_MAP.getItemMap().get(item)) + ("]"));
+	}
+	public static void printGivePlayerItemMessageOutput(String item, int amount, Player player) {
+		if (BLOCK_MAP.getBlockMap().get(item) != null)
+			System.out.println(("Gave ") + (amount) + (" ") + ("[") + (BLOCK_MAP.getBlockMap().get(item)) + ("] to ") + (player.getName()));
+		else
+			System.out.println(("Gave ") + (amount) + (" ") + ("[") + (ITEM_MAP.getItemMap().get(item)) + ("] to ") + (player.getName()));
+	}
 	public static void printTimeMessageOutput(String timeMode, World world) {
 		switch (timeMode) {
 			case "set" -> System.out.println("Set the time to " + world.getGameTicks());
@@ -85,7 +95,10 @@ public class CommandOutputMessage {
 	}
 	
 	public static void printUnknownItemMessageOutput(String itemName) {
-		System.out.println("Unknown item 'minecraft:" + (itemName) + "'");
+		if (!itemName.startsWith("minecraft:"))
+			System.out.println("Unknown item '" + (itemName) + "'");
+		else
+			System.out.println("Unknown item 'minecraft:" + (itemName) + "'");
 	}
 	
 	public static void printUnknownCommandMessageOutput() {
