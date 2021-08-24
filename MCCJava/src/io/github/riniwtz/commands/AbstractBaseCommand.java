b05d64d1fe2425ed176200c2e824be3164030550
riniwtz <riniwtzcode@gmail.com>
2021-08-24 13:49:43 +0800

    Update and Refactor

--- MCCJava/src/io/github/riniwtz/commands/AbstractBaseCommand.java
+++ MCCJava/src/io/github/riniwtz/commands/AbstractBaseCommand.java
@@ -5,10 +5,36 @@ import io.github.riniwtz.mcc.Player;
 import io.github.riniwtz.mcc.World;
 
 public abstract class AbstractBaseCommand {
-	protected Blocks block = new Blocks();
-	protected World world = new World();
-	protected Player player = new Player();
-	protected Items item = new Items();
-	public abstract void execute(String[] cmd);
-	protected abstract boolean hasCommandHandlerError(String[] cmd);
+    protected String[] cmd;
+    protected Player player;
+    protected Blocks block;
+    protected Items item;
+    protected World world;
+
+    protected AbstractBaseCommand(String[] cmd) {
+        this.cmd = cmd;
+    }
+
+    protected AbstractBaseCommand(String[] cmd, World world) {
+        this.cmd = cmd;
+        this.world = world;
+    }
+
+    protected AbstractBaseCommand(String[] cmd, Player player) {
+        this.cmd = cmd;
+        this.player = player;
+    }
+
+    protected AbstractBaseCommand(String[] cmd, Player player, Blocks block) {
+        this.cmd = cmd;
+        this.player = player;
+        this.block = block;
+    }
+
+    protected AbstractBaseCommand(String[] cmd, Player player, Blocks block, Items item) {
+        this.cmd = cmd;
+        this.player = player;
+        this.block = block;
+        this.item = item;
+    }
 }
